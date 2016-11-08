00fcb136f406f1e3fe45364a608321ef032a5236
Rhapsody <rhapsody@rhapsody-mbp.local>
2016-11-08 11:10:47 +0900

    first commit

---
+++ app/metadata.rb
@@ -0,0 +1,10 @@
+name             'app'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures app'
+long_description 'Installs/Configures app'
+version          '0.1.0'
+
+depends "play2", "~> 0.1.0" 
+
